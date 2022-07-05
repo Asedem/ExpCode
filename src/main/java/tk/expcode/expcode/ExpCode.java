41aaefdcb9fd9140a359ef561a043281f5536db1
Asedem <simon.oberzier@juniorjob.de>
2022-07-05 23:52:04 +0200

    The basic implementation of the PluginAPI

---
+++ src/main/java/tk/expcode/expcode/ExpCode.java
@@ -0,0 +1,16 @@
+package tk.expcode.expcode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExpCode {
+
+    private final JavaPlugin main;
+
+    public ExpCode(JavaPlugin main) {
+        this.main = main;
+    }
+
+    public static void initialize() {
+
+    }
+}
